d228a1121850e08a1aab2af2c865a2cd5ef21449
En-Jhih Lo <75471051+ehjhihlo@users.noreply.github.com>
2022-05-03 14:28:09 +0800

    Create 0263. Ugly Number.py

---
+++ 0263. Ugly Number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        # 用遞回來解
+        if n <= 0:
+            return False
+        if n==1:
+            return True
+        
+        if n%2 == 0:
+            return self.isUgly(n/2)
+        if n%3 == 0:
+            return self.isUgly(n/3)
+        if n%5 == 0:
+            return self.isUgly(n/5)
+        
+        return False

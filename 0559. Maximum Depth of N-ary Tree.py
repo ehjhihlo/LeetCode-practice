64a3824c73cae34280223a2dbde67f985013ebd1
En-Jhih Lo <75471051+ehjhihlo@users.noreply.github.com>
2022-07-15 11:40:47 +0800

    Update 0559. Maximum Depth of N-ary Tree.py

--- 0559. Maximum Depth of N-ary Tree.py
+++ 0559. Maximum Depth of N-ary Tree.py
@@ -5,7 +5,7 @@ class Node:
         self.val = val
         self.children = children
 """
-
+# DFS
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
         if not root:
@@ -13,4 +13,21 @@ class Solution:
         if not root.children:
             return 1
         depth = 1 + max(self.maxDepth(child) for child in root.children)
+        return depth
+
+# BFS 
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        depth = 0
+        queue = collections.deque()
+        queue.append(root)
+        
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                for child in node.children:
+                    queue.append(child)
+            depth += 1
         return depth
